27c94e3becfb4d581ccc4bd30b063c883bf4abb2
Leart23 <leartmusahu@gmail.com>
2024-10-22 18:01:41 +0200

    variables

---
+++ site.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php 
+    $characterName = "John";
+    $characterAge = 35;
+    echo("Hello World and $characterName $characterAge <br>" );
+    $characterName = "Tom";
+    echo("Hello World and $characterName $characterAge" )
+?>
+    
+</body>
+</html>
