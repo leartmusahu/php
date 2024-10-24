df8f26ac0fc7ddd9323f0d7dd70bd5c399c981fd
Leart23 <leartmusahu@gmail.com>
2024-10-24 14:51:30 +0200

    associative arrays

--- site.php
+++ site.php
@@ -6,23 +6,15 @@
     <title>Document</title>
 </head>
 <body>
-   
-   <form action="site.php" method="GET">
-    Color: <input type="text" name="color"> <br>
-    Plural Noun: <input type="text" name="pluralNoun"> <br>
-    Celebrity: <input type="text" name="celebrity"> <br>
-   <input type="submit">
-</form>
-<br>
+    <form action="site.php" method="POST">
+        <input type="text" name="student">
+        <input type="submit">
+    </form>
 
-<?php 
-    $color = $_GET["color"];
-    $pluralNoun = $_GET["pluralNoun"];
-    $celebrity = $_GET["celebrity"];
-    echo "Roses are $color <br>";
-    echo "$pluralNoun are blue <br>";
-    echo "I love $celebrity <br>";
-?>
+    <?php 
+        $grades = ["Jim"=>"A+", "Pam"=>"B-","John"=>"C+"];
+        echo $grades[$_POST["student"]];
+    ?>
 
 </body>
 </html>
