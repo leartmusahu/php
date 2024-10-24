6c1962502465eb3b702568a3983f3783723ed236
Leart23 <leartmusahu@gmail.com>
2024-10-24 14:04:56 +0200

    Mad Libs game

--- site.php
+++ site.php
@@ -8,13 +8,21 @@
 <body>
    
    <form action="site.php" method="GET">
-    <input type="number" name="num1">
-    <br>
-    <input type="number" name="num2">
-    <input type="submit">
-    <br>
-   </form>
-   Answer:
-<?php echo $_GET["num1"] + $_GET["num2"] ?>
+    Color: <input type="text" name="color"> <br>
+    Plural Noun: <input type="text" name="pluralNoun"> <br>
+    Celebrity: <input type="text" name="celebrity"> <br>
+   <input type="submit">
+</form>
+<br>
+
+<?php 
+    $color = $_GET["color"];
+    $pluralNoun = $_GET["pluralNoun"];
+    $celebrity = $_GET["celebrity"];
+    echo "Roses are $color <br>";
+    echo "$pluralNoun are blue <br>";
+    echo "I love $celebrity <br>";
+?>
+
 </body>
 </html>
